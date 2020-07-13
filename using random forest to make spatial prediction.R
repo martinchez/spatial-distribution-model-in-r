f63fa954f34793972d31efaab9836ccad8228683
martinchez <martinchege1998@gmail.com>
2020-07-13 16:16:11 +0300

    fist commit

--- using random forest to make spatial prediction.R
+++ using random forest to make spatial prediction.R
@@ -4,4 +4,14 @@ bf
 
 dim(bf)
 head(bf)
-plot(bf)
+plot(bf[,1:2],cex=0.4,col='red')
+#install.packages("maptools")
+library(maptools)
+data(wrld_simpl)
+plot(wrld_simpl,add=TRUE)
+
+
+##predictors
+library(raster)
+wc<- raster::getData('worldclim',res=10,var='bio')
+plot(wc[[c(1,12)]],nr=2)
